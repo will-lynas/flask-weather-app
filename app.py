9b95506aebafc39272403c3a496c81e855b679fa
Artem Streltsov <artyomstreltsov@mail.ru>
2023-10-16 15:33:21 +0100

    Add caching

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ db = SQLAlchemy(app)
 
 class WeatherData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
     temp = db.Column(db.Float, nullable=False)
     time = db.Column(db.Float, nullable=False)
 
@@ -43,8 +45,19 @@ def index():
 
 @app.route('/<country_name>/<city_name>')
 def city_weather(country_name, city_name):
-    result = api_fetch(country_name, city_name)
-    return f"Country: {country_name}. City: {city_name}. Temperature: {result.temp}."
+    last_result = db.session.query(WeatherData).filter(WeatherData.country.like(country_name), WeatherData.city.like(city_name)).order_by(WeatherData.time.desc()).first()
+    if last_result:
+        last_fetch = last_result.time
+        last_fetch_delta = time.time() - last_fetch
+        # Only fetch more frequently than once every cache_time seconds
+        cache_time = 10
+        if last_fetch_delta > cache_time:
+            last_result = api_fetch(country_name, city_name)
+            last_fetch_delta = 0
+    else:
+        last_result = api_fetch(country_name, city_name)
+        last_fetch_delta = 0
+    return f"Country: {country_name}. City: {city_name}. Temperature: {last_result.temp}. Time since updated: {last_fetch_delta}"
 
 
 def api_fetch(country_name, city_name) -> WeatherData:
@@ -57,7 +70,7 @@ def api_fetch(country_name, city_name) -> WeatherData:
         }
     endpoint = "http://api.openweathermap.org/data/2.5/weather"
     response = requests.get(endpoint, params=params).json()
-    weather_data = WeatherData(temp=response["main"]["temp"], time=time.time())
+    weather_data = WeatherData(country=country_name, city=city_name, temp=response["main"]["temp"], time=time.time())
     db.session.add(weather_data)
     db.session.commit()
     return weather_data
