cc04f6d613297390f85f8effe7302fd95cacade2
Will Lynas <lynaswill@gmail.com>
2023-10-31 15:52:27 +0000

    Add docker tests

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect
-import requests
-from flask_sqlalchemy import SQLAlchemy
 import os
 import time
 
+from flask import Flask, redirect
+import requests
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.wrappers import Response
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,41 +15,41 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class CityNotFoundError(Exception):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("City not found")
 
-class WeatherData(db.Model):
+class WeatherData(db.Model): # type: ignore[name-defined,misc] # pylint: disable=too-few-public-methods
     id = db.Column(db.Integer, primary_key=True)
     country = db.Column(db.String, nullable=False)
     city = db.Column(db.String, nullable=False)
     temp = db.Column(db.Float, nullable=False)
     time = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__name__} {self.temp} {self.time} {self.country} {self.city}>"
-
-
 
 with app.app_context():
     db.create_all()
 
-
 @app.route('/')
-def index():
+def index() -> Response:
     return redirect('/UK/London')
 
-
 @app.route('/<country_name>/<city_name>')
-def city_weather(country_name, city_name): 
+def city_weather(country_name: str, city_name: str) -> str | tuple[str, int]:
     try:
         result = api_fetch(country_name, city_name)
     except CityNotFoundError:
         return 'City not Found', 404
-    return f"Country: {country_name}. City: {city_name}. Temperature: {result.temp}. Time since updated: {time.time() - result.time}"
+    return (f"Country: {country_name}. City: {city_name}. Temperature: {result.temp}. "
+            f"Time since updated: {time.time() - result.time}")
 
 
-def api_fetch(country_name, city_name) -> WeatherData:
-    last_result = db.session.query(WeatherData).filter(WeatherData.country == country_name, WeatherData.city == city_name).order_by(WeatherData.time.desc()).first()
+def api_fetch(country_name: str, city_name: str) -> WeatherData:
+    last_result = db.session.query(WeatherData).filter(
+            WeatherData.country == country_name,
+            WeatherData.city == city_name
+            ).order_by(WeatherData.time.desc()).first()
     if last_result:
         last_fetch = last_result.time
         last_fetch_delta = time.time() - last_fetch
@@ -56,7 +57,7 @@ def api_fetch(country_name, city_name) -> WeatherData:
         if last_fetch_delta <= cache_time:
             return last_result
 
-    with open('api_key.txt') as f:
+    with open('api_key.txt', encoding="utf-8") as f:
         api_key = f.read().strip()
     params = {
             "q": f"{city_name}, {country_name}",
@@ -64,11 +65,15 @@ def api_fetch(country_name, city_name) -> WeatherData:
             "units": "metric"
         }
     endpoint = "http://api.openweathermap.org/data/2.5/weather"
-    response = requests.get(endpoint, params=params)
+    response = requests.get(endpoint, params=params, timeout=5)
     if not response:
         raise CityNotFoundError
     response_json = response.json()
-    weather_data = WeatherData(country=country_name, city=city_name, temp=response_json["main"]["temp"], time=time.time())
+    weather_data = WeatherData(
+            country=country_name,
+            city=city_name,
+            temp=response_json["main"]["temp"],
+            time=time.time())
     db.session.add(weather_data)
     db.session.commit()
     return weather_data
